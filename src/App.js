5a7bf2b9696bc12bc21224c81ed08168b1bba547
Talha Malik <talhamalik25.tm@gmail.com>
2024-04-24 16:41:00 +0500

    Completed project as questions and answers are shown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const questions = [
+	{
+		id: 3457,
+		question: "What language is React based on?",
+		answer: "JavaScript",
+	},
+	{
+		id: 7336,
+		question: "What are the building blocks of React apps?",
+		answer: "Components",
+	},
+	{
+		id: 8832,
+		question: "What's the name of the syntax we use to describe a UI in React?",
+		answer: "JSX",
+	},
+	{
+		id: 1297,
+		question: "How to pass data from parent to child components?",
+		answer: "Props",
+	},
+	{
+		id: 9103,
+		question: "How to give components memory?",
+		answer: "useState hook",
+	},
+	{
+		id: 2002,
+		question:
+			"What do we call an input element that is completely synchronised with state?",
+		answer: "Controlled element",
+	},
+];
+
+export default function App ()
+{
+    return <div className='App'>
+        <FlashCards />
     </div>
-  );
 }
 
-export default App;
+const FlashCards = () =>
+{
+    // State for current question and answer being shown
+    const [ textShownId, setTextShownId ] = useState( null );
+    const handleClick = ( id ) =>
+    {
+        setTextShownId(id !== textShownId ? Number(id): null);
+    }
+    return <div className='flashcards'>
+        { questions.map( question => (
+            <Card question={question} textShownId={textShownId} handleClick={handleClick} key={question.id} />
+        ))}
+    </div>
+};
+
+const Card = ({question, textShownId, handleClick}) =>
+{
+    return <div className={question.id === textShownId ? 'selected' : ''} onClick={() => handleClick(Number(question.id))}>
+                <p>{ question.id === textShownId ? question.answer : question.question }</p>
+            </div>
+}
